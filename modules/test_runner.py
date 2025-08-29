44118c4cd4efc9c73e79ffaf2172b9cbcf21df51
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-29 23:12:16 +1000

    Update test_runner.py

--- modules/test_runner.py
+++ modules/test_runner.py
@@ -1,1 +1,9 @@
 
+import pandas as pd
+
+def update_status(df: pd.DataFrame, case_id: str, status: str, comments: str = ""):
+    df.loc[df["ID"] == case_id, ["Status", "Comments"]] = [status, comments]
+    return df
+
+def get_suite_cases(df: pd.DataFrame, suite: str):
+    return df[df["Suite"] == suite]
