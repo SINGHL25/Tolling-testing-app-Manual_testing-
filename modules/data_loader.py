1d34590818c387c81faa23e3092337d5d00c3d78
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-29 23:12:03 +1000

    Update data_loader.py

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -1,1 +1,8 @@
+import pandas as pd
+
+def load_testcases(file_path: str) -> pd.DataFrame:
+    return pd.read_csv(file_path)
+
+def save_testcases(df: pd.DataFrame, file_path: str):
+    df.to_csv(file_path, index=False)
 
